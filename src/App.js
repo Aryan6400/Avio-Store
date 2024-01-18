c9a4937ea9c7dda8ea5470dd2121d028f7f57a43
Aryan Singh <74493183+Aryan6400@users.noreply.github.com>
2024-01-18 15:54:24 +0530

    Implemented code splitting

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
+import { useAuth } from "./context/AuthContext";
 
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import Home from "./components/Home/Home";
-import Category from "./components/Category/Category";
-import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Newsletter from "./components/Footer/Newsletter/Newsletter";
-import SignUp from "./components/Auth/Signup/SignUp";
-import Login from "./components/Auth/Login/Login";
-import Cart from "./components/Cart/Cart";
-import About from "./components/About/About";
 import PrivateComponent from "./components/PrivateComponent";
-import { useAuth } from "./context/AuthContext";
-import SavedForLater from "./components/SavedForLater/SavedForLater";
+
+const Cart = lazy(() => import("./components/Cart/Cart"));
+const Home = lazy(() => import("./components/Home/Home"));
+const Category = lazy(() => import("./components/Category/Category"));
+const SingleProduct = lazy(() => import("./components/SingleProduct/SingleProduct"));
+const About = lazy(() => import("./components/About/About"));
+const SavedForLater = lazy(() => import("./components/SavedForLater/SavedForLater"));
+const Login = lazy(() => import("./components/Auth/Login/Login"));
+const SignUp = lazy(() => import("./components/Auth/Signup/SignUp"));
+
 
 function App() {
-    const {setAuth} = useAuth();
-    useEffect(()=>{
+    const { setAuth } = useAuth();
+    useEffect(() => {
         const auth1 = localStorage.getItem("User");
-        if(auth1) setAuth(true);
+        if (auth1) setAuth(true);
         else setAuth(false);
     }, [])
 
@@ -29,17 +31,43 @@ function App() {
 
             <Header />
             <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/category/:id" element={<Category />} />
-                <Route path="/product/:id" element={<SingleProduct />} />
-                <Route path="/signup" element={<SignUp />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/about" element={<About />} />
+                <Route path="/" element={
+                    <Suspense fallback={<p>Loading...</p>}>
+                        <Home />
+                    </Suspense>} />
+                <Route path="/category/:id" element={
+                    <Suspense fallback={<p>Loading...</p>}>
+                        <Category />
+                    </Suspense>} />
+                <Route path="/product/:id" element={
+                    <Suspense fallback={<p>Loading...</p>}>
+                        <SingleProduct />
+                    </Suspense>} />
+                <Route path="/signup" element={
+                    <Suspense fallback={<p>Loading...</p>}>
+                        <SignUp />
+                    </Suspense>} />
+                <Route path="/login" element={
+                    <Suspense fallback={<p>Loading...</p>}>
+                        <Login />
+                    </Suspense>} />
+                <Route path="/about" element={
+                    <Suspense fallback={<p>Loading...</p>}>
+                        <About />
+                    </Suspense>} />
 
 
                 <Route element={<PrivateComponent />}>
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/saved" element={<SavedForLater />} />
+                    <Route path="/cart" element={
+                        <Suspense fallback={<p>Loading...</p>}>
+                            <Cart />
+                        </Suspense>
+                    } />
+                    <Route path="/saved" element={
+                        <Suspense fallback={<p>Loading...</p>}>
+                            <SavedForLater />
+                        </Suspense>
+                    } />
                 </Route>
             </Routes>
             <Newsletter />
